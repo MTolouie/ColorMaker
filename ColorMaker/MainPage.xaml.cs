2a9fc751446d30098c4aefeb9cffd116b2d9e9e4
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-06-14 13:18:55 +0300

    BackgroundColor Changes

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -2,16 +2,30 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-    private void sldRed_ValueChanged(object sender, ValueChangedEventArgs e)
+    public MainPage()
     {
+        InitializeComponent();
+    }
 
+    private void sld_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        var red = sldRed.Value;
+        var blue = sldBlue.Value;
+        var green = sldGreen.Value;
+
+        Color color = Color.FromRgb(red, blue, green);
+
+        ImplementColor(color);
+
+    }
+
+    private void ImplementColor(Color color)
+    {
+        Container.BackgroundColor = color;
+        btnRandom.BackgroundColor = color;
+        lblHex.Text = $"HEX Value : {color.ToHex()}";
     }
 
     private void btnRandom_Clicked(object sender, EventArgs e)
